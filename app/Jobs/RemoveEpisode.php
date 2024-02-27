137d0982d70d1bdac32a1ba227f1701f19cf0538
Stefan de Groot <stefan@wubdev.nl>
2024-02-27 10:44:11 +0100

    add remove episode job

---
+++ app/Jobs/RemoveEpisode.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Episode;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class RemoveEpisode implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(public Episode $episode)
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $this->episode->sections()->delete();
+
+        $this->episode->whisperJobs()->delete();
+
+        $this->episode->delete();
+    }
+}
